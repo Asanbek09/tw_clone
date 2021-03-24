da1eddc1ac13c3d320883447f34ee3ea91fd5291
Asanbek Samudin uulu <samudinua@gmail.com>
2021-03-24 15:29:31 +0600

    profileRoutes добавлена

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const loginRoute = require('./routes/loginRoutes');
 const registerRoute = require('./routes/registerRoutes');
 const logoutRoute = require('./routes/logout');
 const postRoute = require('./routes/postRoutes');
+const profileRoute = require('./routes/profileRoutes');
 
 // Api Routes
 const postsApiRoute = require('./routes/api/posts');
@@ -36,6 +37,7 @@ app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 app.use("/logout", logoutRoute);
 app.use("/posts", middleware.requireLogin, postRoute);
+app.use("/profile", middleware.requireLogin, profileRoute);
 //url для api
 app.use("/api/posts", postsApiRoute);
 
