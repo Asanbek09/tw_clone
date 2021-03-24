965876489ad5c0fe3f9c2191bdaacb18387ac38e
Asanbek Samudin uulu <samudinua@gmail.com>
2021-03-24 15:58:27 +0600

    main.css и profilePage.pug изменены

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -7,40 +7,44 @@ const User = require('../models/UserSchema');
 
 router.get("/", (req, res, next) => {
 
-    var payload = {
-        pageTitle: req.session.user.username,
-        userLoggedIn: req.session.user,
-        userLoggedInJs: JSON.stringify(req.session.user),
-        profileUser: req.session.user
-    }
+  var payload = {
+    pageTitle: req.session.user.username,
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+    profileUser: req.session.user
+  }
     
-    res.status(200).render("profilePage", payload);
+  res.status(200).render("profilePage", payload);
 })
 
 router.get("/:username", async (req, res, next) => {
 
-    var payload = await getPayload(req.params.username, req.session.user);
+  var payload = await getPayload(req.params.username, req.session.user);
     
-    res.status(200).render("profilePage", payload);
+  res.status(200).render("profilePage", payload);
 })
 
 async function getPayload(username, userLoggedIn) {
-    var user = await User.findOne({ username: username })
+  var user = await User.findOne({ username: username })
+
+  if(user == null) {
+    user = await User.findById(username);
 
     if(user == null) {
-        return {
-            pageTitle: "User not found",
-            userLoggedIn: userLoggedIn,
-            userLoggedInJs: JSON.stringify(userLoggedIn)
-        }
-    }
-
-    return {
-        pageTitle: user.username,
+      return {
+        pageTitle: "User not found",
         userLoggedIn: userLoggedIn,
-        userLoggedInJs: JSON.stringify(userLoggedIn),
-        profileUser: user
+        userLoggedInJs: JSON.stringify(userLoggedIn)
+      }
     }
+  }
+
+  return {
+    pageTitle: user.username,
+    userLoggedIn: userLoggedIn,
+    userLoggedInJs: JSON.stringify(userLoggedIn),
+    profileUser: user
+  }
 }
 
 module.exports = router;
